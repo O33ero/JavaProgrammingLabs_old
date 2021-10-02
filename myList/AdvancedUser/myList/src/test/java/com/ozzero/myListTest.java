6a0dc2148b779a094c574a7efd7f42da657ada62
O33ero <58762519+O33ero@users.noreply.github.com>
2021-10-02 16:17:12 +0300

    Edited algorith of myList, added Iterator

--- myList/AdvancedUser/myList/src/test/java/com/ozzero/myListTest.java
+++ myList/AdvancedUser/myList/src/test/java/com/ozzero/myListTest.java
@@ -1,11 +1,15 @@
 package com.ozzero;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Iterator;
 
 import org.junit.Test;
 
 public class myListTest {
-
+    
     @Test
     public void testGet_1() {
         myList<Integer> lst = new myList<>(5);
@@ -161,6 +165,28 @@ public class myListTest {
     }
 
     @Test
+    public void testErase_2() {
+        myList<Integer> lst = new myList<>(5);
+        lst.set(0, 1);
+        lst.set(1, 2);
+        lst.set(2, 3);
+        lst.set(3, 4);
+        lst.set(4, 5);
+
+        int t = lst.erase(1);
+        assertEquals(0, t);
+        assertEquals(4, lst.size());
+
+        t = lst.erase(5);
+        assertEquals(3, t);
+        assertEquals(3, lst.size());
+
+        t = lst.erase(2);
+        assertEquals(0, t);
+        assertEquals(2, lst.size());
+    }
+
+    @Test
     public void testClear_1() {
         myList<Integer> lst = new myList<>(5);
         lst.set(0, 1);
@@ -172,5 +198,46 @@ public class myListTest {
         lst.clear();
         assertEquals(0, lst.size());
 
+    }
+
+    @Test
+    public void testIterator_1() {
+        myList<Integer> lst = new myList<>(5);
+        lst.set(0, 1);
+        lst.set(1, 2);
+        lst.set(2, 3);
+        lst.set(3, 4);
+        lst.set(4, 5);
+
+        Iterator<Integer> iter = lst.iterator();
+
+        Integer a = iter.next(); // 1
+        assertTrue(a.equals(1)); 
+
+        Integer b = iter.next(); // 2
+        b = iter.next(); // 3
+        b = iter.next(); // 4
+        assertTrue(b.equals(4));
+
+        b = iter.next(); // 5
+
+        assertFalse(iter.hasNext());
+    }
+
+
+    @Test
+    public void testIterator_2() {
+        myList<Integer> lst = new myList<>(5);
+        lst.set(0, 1);
+        lst.set(1, 2);
+        lst.set(2, 3);
+        lst.set(3, 4);
+        lst.set(4, 5);
+
+        int i = 0;
+        for(Integer now : lst) {
+            assertTrue(now.equals(lst.get(i)));
+            i++;
+        }
     }
 }
